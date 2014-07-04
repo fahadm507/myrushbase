89b352b19d1c5d5678a88a09a81f83a7a2704447
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-03 22:42:52 -0400

    add small changes on spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     sequence(:email) { |n| "#{n}@example.com" }
     first_name "Fahad"
     last_name "Mukunzi"
-    password "testing123"
-    password_confirmation "testing123"
+    password "testing2020"
+    password_confirmation "testing2020"
   end
 end
