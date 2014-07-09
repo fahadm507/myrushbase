6d0d355f20751395bfcd9e6892b93d6d12c6189b
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-09 16:33:09 -0400

    Add changes to members show page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,14 @@
 class MembersController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @members = User.order(created_at: :desc).limit(10)
   end
 
   def show
-    @member = User.find(params[:id])
+    @user = User.find(params[:id])
+    @post = Post.new
+    @comment= Comment.new
+    @like = Like.new
+    @point = Point.new
   end
 end
