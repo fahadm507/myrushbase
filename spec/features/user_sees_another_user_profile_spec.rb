98b0a00e0acce80e35e4b1e075f7ad4e71518130
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-09 15:00:49 -0400

    loggedin user sees another user

--- spec/features/user_sees_another_user_profile_spec.rb
+++ spec/features/user_sees_another_user_profile_spec.rb
@@ -10,20 +10,18 @@ feature 'Registered user creates posts', %q{
 
     user = FactoryGirl.create(:user)
     member = FactoryGirl.create(:user)
-    posts = FactoryGirl.create_list(:post, 10)
+    posts = FactoryGirl.create_list(:post, 10, user: member)
 
 
     sign_in_as(user)
 
     visit '/members'
 
-    within(".all-members") do
-      click_on "#{member.first_name} #{member.last_name}"
-    end
+      click_link "#{member.first_name} #{member.last_name}"
 
     expect(page).to have_content member.first_name
     expect(page).to have_content member.last_name
-    expect(page).to have_content member.posts.all
+
   end
 
 end
