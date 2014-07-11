ee5d65aa0bb5ee39819138ae7e93697981967fcf
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-11 14:17:18 -0400

    add followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
   has_many :followers, foreign_key: :following_user_id
-
   has_many :reverse_followers, foreign_key: :followed_user_id, class_name: "Follower"
 
   has_many :followed_users, through: :followers, foreign_key: :followed_user_id
-
   has_many :following_users, through: :reverse_followers, foreign_key: :following_user_id
 
   belongs_to :category
