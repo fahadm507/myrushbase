1ab55d1bea7aac71959659bef2609423b49b623a
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-14 11:13:08 -0400

    add following/followers views

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   has_many :reverse_relationships, foreign_key: "followed_id",
             class_name: 'Relationship', dependent: :destroy
-  has_many : followers, through: :reverse_relationships, source: :follower
+  has_many :followers, through: :reverse_relationships, source: :follower
 
 
   belongs_to :category
