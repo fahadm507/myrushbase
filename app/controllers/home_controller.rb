b10c9f8f234fd235593b946de11e34ce22b585ee
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-17 23:42:34 -0400

    Add most recent users to landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @recent_memebers = User.last(6)
     if user_signed_in?
       redirect_to controller: 'users', action: 'index'
     end
