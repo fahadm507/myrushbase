ac79949a905951fc7dfa0004ec564bd646ab658f
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-21 10:49:17 -0400

    clean up user relationships

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,20 +3,21 @@ class PointsController < ApplicationController
     @user = User.find(params[:user_id])
     @point = @user.points.build(user_id: current_user.id)
     if @point.save
-      redirect_to users_path
+      redirect_to user_path(@user)
     else
       flash[:notice] = "un able to save this like"
-      redirect_to users_path
+      redirect_to user_path(@user)
     end
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     @point = Point.find(params[:id])
     if @point.destroy
-      redirect_to users_path
+      redirect_to user_path(@user)
     else
       flash[:notice]= "like could not be deleted"
-      redirect_to users_path
+      redirect_to user_path(@user)
     end
   end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,15 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
-  respond_to :html, :js
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_with @user
+    redirect_to user_path(@user)
   end
 
   def destroy
     @user =Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_with @user
+    redirect_to user_path(@user)
   end
 end
