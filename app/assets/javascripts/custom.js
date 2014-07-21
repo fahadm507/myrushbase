abbe1908b1248d84bd706827cdb77d6f0c01a963
Fahad Mukunzi <fahadm507@gmail.com>
2014-07-21 16:08:51 -0400

    refoctoring

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -24,34 +24,34 @@ $(document).ready(function() {
     event.preventDefault();
     $('#main-nav').toggleClass("open");
   });
-  $('.comment-form').on('ajax:success', function(e, data, textStatus, jqXHR){
-    $(this).closest(".comments-details").toggleClass("text-center")
-    console.log(data.description);
-  });
+    // $('.comment-form').on('ajax:success', function(e, data, textStatus, jqXHR){
+    //   $(this).closest(".comments-details").toggleClass("text-center")
+    //   console.log(data.description);
+    // });
 
-  $('.post').on('submit', '.comment-form', function(event) {
-    event.preventDefault();
+    // $('.post').on('submit', '.comment-form', function(event) {
+    //   event.preventDefault();
 
-    var $post = $(event.delegateTarget);
+    //   var $post = $(event.delegateTarget);
 
-    var action = $(this).attr('action');
-    var data = $(this).serialize();
+    //   var action = $(this).attr('action');
+    //   var data = $(this).serialize();
 
-    $.ajax({
-      url: action,
-      method: 'POST',
-      data: data,
-      success: function(data) {
-        $post.find('#comment-for-post').append('<li>' + data.description + '</li>');
-        $post.find('#comment_description').val('');
-        currentLikesCount = parseInt($post.find('.likes-count').text(), 10);
-        $post.find('.likes-count').text(currentLikesCount + 1);
-      },
-      error: function(errors) {
+    //   $.ajax({
+    //     url: action,
+    //     method: 'POST',
+    //     data: data,
+    //     success: function(data) {
+    //       $post.find('#comment-for-post').append('<li>' + data.description + '</li>');
+    //       $post.find('#comment_description').val('');
+    //       currentLikesCount = parseInt($post.find('.likes-count').text(), 10);
+    //       $post.find('.likes-count').text(currentLikesCount + 1);
+    //     },
+    //     error: function(errors) {
 
-      }
-    });
-  });
+    //     }
+    //   });
+    // });
 });
 // scroll function
 function scrollToID(id, speed){
